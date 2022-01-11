8d608416e9498cb522268f7b83285f7e723c1e55
Esmeralda Vera <81045242+EsmeraldaVera@users.noreply.github.com>
2022-01-11 13:27:29 -0600

    view user completed

--- revature-bookshelf-frontend/src/app/user-profile/user-profile.component.ts
+++ revature-bookshelf-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 
 
@@ -11,25 +13,43 @@ import { UserService } from '../user.service';
 })
 
 export class UserProfileComponent implements OnInit {
-  currentUser: Object = {};
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    middleName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+  });
+
+
+  email: string | null = null
+  // currentUser: Object = {};
+    currentUser: any = {}
 
   constructor(
     public userService: UserService,
-    private actRoute: ActivatedRoute
-  ) {
-    let id = this.actRoute.snapshot.paramMap.get('id');
-    console.log("this is my output:" + id)
-    this.userService.getUserProfile(id).subscribe(res => {
-      this.currentUser = res.msg;
-    })
+    private actRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) 
+  
+  {
+    // let id = this.actRoute.snapshot.paramMap.get('id');
+    //   this.userService.getUserProfile(id).subscribe(res => {
+    //   this.currentUser = res.msg;
+    // })
   }
 
-  ngOnInit() {
-    console.log(this.actRoute.snapshot.paramMap.get('token'));
+  updateProfile(){
+    console.log(this.profileForm.value)
+  }
 
-    this.actRoute.paramMap.subscribe(paramMap => {
-       console.log(paramMap.get('token'));
-    });
- }
+
+  ngOnInit() {
+    this.userService.decodeToken();
+    this.email = this.userService.userName;
+    this.userService
+          .getUser()
+          .subscribe(user => this.currentUser = user);
+ 
+      }      
 
 }
--- revature-bookshelf-frontend/src/app/user.service.ts
+++ revature-bookshelf-frontend/src/app/user.service.ts
@@ -24,6 +24,7 @@ export class UserService {
             const decoded: any = jwt_decode(token);
             this.userName = decoded.sub;
         }
+        return this.userName;
     }
 
     doLogin(credentials: any) {
@@ -84,29 +85,9 @@ export class UserService {
 
     constructor(private httpClient: HttpClient) {}
 
-    getUserProfile(id : any): Observable<any> {
-        let api = `${this.endpoint}/user-profile/${id}`;
-        return this.httpClient.get(api, { headers: this.headers }).pipe(
-          map((response: any) => {
-            const user = response as User;
-            return user || {}
-          }),
-          catchError(this.handleError)
-        )
-      }
-
-    
-
-       // Error 
-  handleError(error: HttpErrorResponse) {
-    let msg = '';
-    if (error.error instanceof ErrorEvent) {
-      // client-side error
-      msg = error.error.message;
-    } else {
-      // server-side error
-      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    getUser() {
+      return this.httpClient.get(`http://localhost:4200/users`); 
     }
-    return throwError(msg);
-  }
+
+
 }

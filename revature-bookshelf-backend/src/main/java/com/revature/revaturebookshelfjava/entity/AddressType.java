72cd08fca01c39f30d00e9b494dd18a4aa1a3f5a
jnguy707 <Jeremy.N.707@live.com>
2022-01-11 10:06:34 -0800

    address overwrite feature added

--- revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/entity/AddressType.java
+++ revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/entity/AddressType.java
@@ -12,11 +12,19 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "address_types")
-public class AddressType {
+public class AddressType implements Comparable<AddressType>{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String type;
+
+    @Override
+    public int compareTo(AddressType o) {
+        if(this.type.equals(o.getType())) {
+            return 0;
+        }
+        return 1;
+    }
 
     // Commented out because of recursive nature
 //    @ManyToMany(mappedBy = "types")

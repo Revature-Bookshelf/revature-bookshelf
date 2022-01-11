e62b17ac2961c8286023a759bf5f4bac8eb139f2
jnguy707 <Jeremy.N.707@live.com>
2022-01-11 02:09:14 -0800

    added GET method for user's information (names) and respective security measures

--- revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/controller/UserController.java
+++ revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/controller/UserController.java
@@ -6,9 +6,15 @@ import com.revature.revaturebookshelfjava.entity.User;
 import com.revature.revaturebookshelfjava.service.CartService;
 import com.revature.revaturebookshelfjava.service.UserService;
 
+import nonapi.io.github.classgraph.json.JSONDeserializer;
+import nonapi.io.github.classgraph.json.JSONSerializer;
+import nonapi.io.github.classgraph.json.JSONUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +35,26 @@ public class UserController {
     // TODO: Finalize URL
     // POST /api/users
     @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/api/users"
+    )
+    public SecureUser getUser() {
+        String username = extractUsername();
+        // Note: some circular mapping of entities of cart and user in foundUser
+        User foundUser = userService.getUser(username);
+
+        // Using Inner Private Class to Encapsulate sensitive information
+        SecureUser securedUser = new SecureUser();
+        securedUser.id = foundUser.getId();
+        securedUser.email = foundUser.getEmail();
+        securedUser.firstName = foundUser.getFirstName();
+        securedUser.middleName = foundUser.getMiddleName();
+        securedUser.lastName = foundUser.getLastName();
+
+        return securedUser;
+    }
+
+    @RequestMapping(
             method = RequestMethod.POST,
             value = "/api/users"
     )
@@ -41,4 +67,17 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
     }
 
+    public String extractUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+
+    private class SecureUser{
+        public int id;
+        public String email;
+        public String firstName;
+        public String middleName;
+        public String lastName;
+    }
 }

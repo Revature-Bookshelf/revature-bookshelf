1033dce93644c4f431138543290588ea6e26ea8f
nlundgren <nilslundus@gmail.com>
2022-01-11 21:00:46 -0600

    added bean to userExtractorDetails

--- revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/authenicator/extractor/UserDetailsExtractor.java
+++ revature-bookshelf-backend/src/main/java/com/revature/revaturebookshelfjava/authenicator/extractor/UserDetailsExtractor.java
@@ -3,7 +3,9 @@ package com.revature.revaturebookshelfjava.authenicator.extractor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserDetailsExtractor {
 
     public String extractUsername() {
